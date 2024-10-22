feba57c47c8a72b25bfa42aab260fc8c378bd2e7
Dimas Cahyo Margono <dimascahyomg855@gmail.com>
2024-10-22 21:04:40 +0700

    06_Interaksi Pengguna

---
+++ 06_Interaksi_Pengguna/TP/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Latihan BottomNavigationBar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue, // Mengubah warna tema menjadi biru
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _pages = <Widget>[
+    Center(child: Text('Ini Halaman Beranda')),
+    Center(child: Text('Ini Halaman Wisata')),
+    Center(child: Text('Ini Halaman Profile')),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Latihan BottomNavigationBar'),
+        backgroundColor: Colors.blue, // Mengubah warna AppBar menjadi biru
+      ),
+      body: _pages.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Beranda',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Wisata',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor:
+            Colors.blue, // Mengubah warna item yang dipilih menjadi biru
+        unselectedItemColor:
+            Colors.grey, // Warna item yang tidak dipilih tetap abu-abu
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
